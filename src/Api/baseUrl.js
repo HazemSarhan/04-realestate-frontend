365e59d72bd47bba36f1050bcef2fff7bde09bf9
amd200 <72322072+amd200@users.noreply.github.com>
2024-12-31 17:26:52 +0200

    Update baseUrl.js

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = axios.create({
-  baseURL: 'https://zero4-realestate-backend.onrender.com/',
+  baseURL: 'https://zero4-realestate-backend.onrender.com',
 });
 
 export default baseUrl;
