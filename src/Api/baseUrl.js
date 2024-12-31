72ed77a824e9bf1018773fdccd6df5e22a488d44
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2024-12-31 17:24:44 +0300

    backend url added

--- src/Api/baseUrl.js
+++ src/Api/baseUrl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = axios.create({
-  baseURL: 'https://zero4-realestate-api.onrender.com',
+  baseURL: 'https://zero4-realestate-backend.onrender.com',
 });
 
 export default baseUrl;
